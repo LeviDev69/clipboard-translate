97110f6f470d82825398badde69d78d7907a8012
LeviDev69 <cacheplays247@gmail.com>
2025-05-25 19:56:12 -0600

    Added error handeling and quiting

--- Main.py
+++ Main.py
@@ -2,20 +2,25 @@ import pyperclip
 from deep_translator import GoogleTranslator
 from time import sleep
 from plyer import notification
-
-last_clip = ""
-
-while True:
-    clip = pyperclip.paste()
-    stripped_clip = clip.strip()
-    if stripped_clip != last_clip and stripped_clip != "":
-        last_clip = stripped_clip
-        translated = GoogleTranslator(source='auto', target='en').translate(stripped_clip)
-        notification.notify(
-            title="Translation:",
-            message=translated,
-            timeout=5
-        )
-    else:
-        pass
-    sleep(1)
+try:
+    last_clip = ""
+    print("Translator active")
+    while True:
+        clip = pyperclip.paste()
+        stripped_clip = clip.strip()
+        if stripped_clip != last_clip and stripped_clip != "":
+            last_clip = stripped_clip
+            translated = GoogleTranslator(source='auto', target='en').translate(stripped_clip)
+            notification.notify(
+                title="Translation:",
+                message=translated,
+                app_name="Clipboard Translator",
+                timeout=5
+            )
+        else:
+            pass
+        sleep(1)
+except KeyboardInterrupt:
+    print("Stopped by user exiting translator")
+except Exception as e:
+    print(f"[!]Error: {e}")
